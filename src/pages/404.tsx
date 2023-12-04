f5cc02bf23a5874f9406619984dec0c0569002fd
deep-hwan <deep@deepcomu.com>
2023-12-04 18:08:02 +0900

    위젯_버전_타입_대규모수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NextRouter, useRouter } from 'next/router';
 
-import { Section, Txt, Spacing, Tab } from '@/_ui_libs/_index';
+import { Section, Txt, Spacing, Tab, Container } from '@/_ui_libs/_index';
 import { colors, borderRadius, MQ, fontSize } from '@/libs/themes/_index';
 
 import SEO from '@/seo.config';
@@ -13,27 +13,29 @@ export default function Error() {
   return (
     <>
       <SEO title="페이지를 찾을 수 없습니다" />
-      <Section padding={{ all: 26 }} crossAlign="center">
-        <Txt as="h4" css={{ [MQ[3]]: { fontSize: fontSize.s26 } }}>
-          페이지를 찾을 수 없습니다
-        </Txt>
-        <Spacing size={12} />
-        <Txt as="p" css={{ color: colors.grey500 }}>
-          아래 버튼을 통해 이전페이지로 이동하세요
-        </Txt>
+      <Section>
+        <Container padding={{ all: 26 }} crossAlign="center">
+          <Txt as="h4" css={{ [MQ[3]]: { fontSize: fontSize.s26 } }}>
+            페이지를 찾을 수 없습니다
+          </Txt>
+          <Spacing size={12} />
+          <Txt as="p" css={{ color: colors.grey500 }}>
+            아래 버튼을 통해 이전페이지로 이동하세요
+          </Txt>
 
-        <Spacing size={24} />
-        <Tab
-          css={{
-            background: colors.keyColor,
-            color: colors.white,
-            padding: '14px 30px',
-            borderRadius: borderRadius.s700,
-          }}
-          onClick={() => router.back()}
-        >
-          뒤로가기
-        </Tab>
+          <Spacing size={24} />
+          <Tab
+            css={{
+              background: colors.keyColor,
+              color: colors.white,
+              padding: '14px 30px',
+              borderRadius: borderRadius.s700,
+            }}
+            onClick={() => router.back()}
+          >
+            뒤로가기
+          </Tab>
+        </Container>
       </Section>
     </>
   );
